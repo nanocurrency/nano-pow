75b2571259f12d8f9b9952f54e02802fc2113233
clemahieu <clemahieu@gmail.com>
2019-05-13 16:17:11 +0200

    Initial commit.

---
+++ include/ssp_pow/pow.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <crypto/highwayhash/highwayhash/sip_hash.h>
+
+#include <array>
+#include <atomic>
+
+namespace ssp_pow
+{
+uint64_t hash (void const * const nonce_a, uint64_t const item_a)
+{
+	return highwayhash::SipHash (*reinterpret_cast<highwayhash::HH_U64 const (*) [2]> (nonce_a), reinterpret_cast<char const *> (&item_a), sizeof (item_a));
+}
+class context
+{
+public:
+	context (unsigned difficulty_a) :
+	threshold ((1ULL << difficulty_a) - 1)
+	{
+		assert (difficulty_a > 0 && difficulty_a < 64 && "Difficulty must be greater than 0 and less than 64");
+	}
+	uint64_t const threshold;
+	static uint64_t constexpr lhs_or_mask { 0x1ULL << 63 };
+	static uint64_t constexpr rhs_and_mask { ~lhs_or_mask };
+public:
+	uint64_t reduce (uint64_t const item_a) const
+	{
+		return item_a & threshold;
+	}
+	uint64_t difficulty (void const * const nonce_a, std::array<uint64_t, 2> const solution_a) const
+	{
+		auto sum (hash (nonce_a, solution_a [0] | lhs_or_mask) + hash (nonce_a, solution_a [1] & rhs_and_mask));
+		return (reduce (sum) == 0) ? 0 - sum : 0;
+	}
+	uint64_t slot (size_t const size_a, uint64_t const item_a) const
+	{
+		auto mask (size_a - 1);
+		assert (((size_a & mask) == 0) && "Slab size is not a power of 2");
+		return item_a & mask;
+	}
+	std::array<uint64_t, 2> search (uint32_t * const slab_a, size_t const size_a, void const * const nonce_a, uint64_t const count = std::numeric_limits<uint64_t>::max (), uint64_t const begin = 0)
+	{
+		auto incomplete (true);
+		std::array<uint64_t, 2> result = { 0, 0 };
+		for (uint64_t current (begin), end (current + count); incomplete && current < end; ++current)
+		{
+			result [0] = current;
+			auto hash_l (hash (nonce_a, current | lhs_or_mask));
+			result [1] = slab_a [slot (size_a, 0 - hash_l)];
+			incomplete = reduce (hash_l + hash (nonce_a, result [1] & rhs_and_mask)) != 0;
+		}
+		return incomplete ? std::array<uint64_t, 2> ({ 0, 0 }) : result;
+	}
+	void fill (uint32_t * const slab_a, size_t const size_a, void const * const nonce_a, uint64_t const count, uint64_t const begin = 0)
+	{
+		for (uint64_t current (begin), end (current + count); current < end; ++current)
+		{
+			slab_a [slot (size_a, hash (nonce_a, current & rhs_and_mask))] = current;
+		}
+	}
+};
+}
---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <boost/program_options.hpp>
+
+#include <gtest/gtest.h>
+
+namespace {
+enum class driver
+{
+	gtest,
+	cpp
+};
+std::ostream & operator << (std::ostream & os, driver const & obj)
+{
+	switch (obj)
+	{
+		case driver::gtest:
+			os << "gtest";
+			break;
+		case driver::cpp:
+			os << "cpp";
+			break;
+	}
+	return os;
+}
+std::istream & operator >> (std::istream & is, driver & obj)
+{
+	std::string text;
+	is >> text;
+	if (text == "gtest")
+	{
+		obj = driver::gtest;
+	}
+	else if (text == "cpp")
+	{
+		obj = driver::cpp;
+	}
+	return is;
+}
+}
+
+int main (int argc, char **argv)
+{
+	boost::program_options::options_description description ("Command line options");
+	description.add_options ()
+	("help", "Print out options")
+	("driver", boost::program_options::value<driver> ()->default_value (driver::gtest), "Specify which test driver to use");
+	boost::program_options::variables_map vm;
+	int result (1);
+	try
+	{
+		boost::program_options::store (boost::program_options::parse_command_line (argc, argv, description), vm);
+		boost::program_options::notify (vm);
+		driver driver_l = driver::gtest;
+		auto driver_opt (vm.find ("driver"));
+		if (driver_opt != vm.end ())
+		{
+			driver_l = driver_opt->second.as<driver>();
+		}
+		switch (driver_l)
+		{
+			case driver::gtest:
+			{
+				testing::InitGoogleTest(&argc, argv);
+				result = RUN_ALL_TESTS();
+				break;
+			}
+			case driver::cpp:
+			{
+				assert (false);
+				break;
+			}
+		}
+	}
+	catch (boost::program_options::error const & err)
+	{
+		std::cerr << err.what () << std::endl;
+	}
+	return result;
+}
