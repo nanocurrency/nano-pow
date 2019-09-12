64373d80169688f89ac5e84c230bd33427596089
Guilherme Lawless <guilherme@nano.org>
2019-09-12 10:33:05 +0100

    Converting the OpenCL program source file every time it is modified

---
+++ ConvertOpenCL.cmake
@@ -0,0 +1,11 @@
+file(READ ${CL_FILE} opencl_program)
+file(WRITE ${CPP_FILE} "#include <string>\n"
+"#ifndef NP_VALUE_SIZE\n"
+"#define NP_VALUE_SIZE 4\n"
+"#endif\n"
+"namespace nano_pow\n"
+"{\n"
+"std::string opencl_program = R\"OCL(\n"
+"${opencl_program}\n"
+")OCL\";\n"
+"}")
