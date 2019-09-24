1879a022c2e2cd928b2d91c69f27e909fd3a569f
Sergey Kroshnin <sergiysw@gmail.com>
2019-09-24 17:33:13 +0300

    Split out 64 bit external difficulty

--- include/nano_pow/driver.hpp
+++ include/nano_pow/driver.hpp
@@ -16,15 +16,6 @@ public:
 	virtual ~driver() = default;
 	virtual void difficulty_set (nano_pow::uint128_t difficulty) = 0;
 	virtual nano_pow::uint128_t difficulty_get () const = 0;
-	void difficulty_set_64 (uint64_t difficulty_a)
-	{
-		auto difficulty (static_cast<nano_pow::uint128_t> (0xffffffffU) << 96 | static_cast<nano_pow::uint128_t> (difficulty_a) << 32);
-		difficulty_set (difficulty);
-	}
-	uint64_t difficulty_get_64 () const
-	{
-		return static_cast<uint64_t> (difficulty_get () >> 32);
-	}
 	virtual void threads_set (unsigned threads) = 0;
 	virtual size_t threads_get () const = 0;
 	// Tell the driver the amount of memory to use, in bytes
